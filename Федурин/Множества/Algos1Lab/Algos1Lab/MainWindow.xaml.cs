c9bf3f2d9b5b3872eba95724e7baa2db079ac23e
denfry <aseraw115@gmail.com>
2024-12-09 03:12:44 +0300

    Добавил 1 и 2 лабы по множествам(с отчетами)

---
+++ Федурин/Множества/Algos1Lab/Algos1Lab/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+
+namespace Algos1Lab
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeSets();
+            DisplaySets();
+        }
+
+        private HashSet<int> _setA, _setB, _setC, _universalSet;
+
+        private void _1_Click(object sender, RoutedEventArgs e)
+        {
+            var result = GetNotA();
+            TextBox1.Text = $"¬A = {{{string.Join(", ", result)}}}";
+        }
+
+        private List<int> GetNotA()
+        {
+            var complementA = new HashSet<int>(_universalSet);
+            complementA.ExceptWith(_setA);
+            var sortedResult = complementA.ToList();
+            sortedResult.Sort();
+            return sortedResult;
+        }
+
+        private void _2_Click(object sender, RoutedEventArgs e)
+        {
+            var result = GetNotB();
+            TextBox2.Text = $"¬B = {{{string.Join(", ", result)}}}";
+        }
+
+        private List<int> GetNotB()
+        {
+            var complementB = new HashSet<int>(_universalSet);
+            complementB.ExceptWith(_setB);
+            var sortedResult = complementB.ToList();
+            sortedResult.Sort();
+            return sortedResult;
+        }
+
+        private void _3_Click(object sender, RoutedEventArgs e)
+        {
+            var result = GetNotC();
+            TextBox3.Text = $"¬C = {{{string.Join(", ", result)}}}";
+        }
+
+        private List<int> GetNotC()
+        {
+            var complementC = new HashSet<int>(_universalSet);
+            complementC.ExceptWith(_setC);
+            var sortedResult = complementC.ToList();
+            sortedResult.Sort();
+            return sortedResult;
+        }
+
+        private void _4_Click(object sender, RoutedEventArgs e)
+        {
+            var result = GetNotAIntersectB();
+            TextBox4.Text = $"¬A ∩ B = {{{string.Join(", ", result)}}}";
+        }
+
+        private List<int> GetNotAIntersectB()
+        {
+            var notA = GetNotA();
+            var intersection = new HashSet<int>(notA);
+            intersection.IntersectWith(_setB);
+            var sortedResult = intersection.ToList();
+            sortedResult.Sort();
+            return sortedResult;
+        }
+
+        private void _5_Click(object sender, RoutedEventArgs e)
+        {
+            var result = GetAIntersectNotC();
+            TextBox5.Text = $"A ∩ ¬C = {{{string.Join(", ", result)}}}";
+        }
+
+        private List<int> GetAIntersectNotC()
+        {
+            var notC = GetNotC();
+            var intersection = new HashSet<int>(_setA);
+            intersection.IntersectWith(notC);
+            var sortedResult = intersection.ToList();
+            sortedResult.Sort();
+            return sortedResult;
+        }
+
+        private void _6_Click(object sender, RoutedEventArgs e)
+        {
+            var result = GetNotBIntersectC();
+            TextBox6.Text = $"¬B ∩ C = {{{string.Join(", ", result)}}}";
+        }
+
+        private List<int> GetNotBIntersectC()
+        {
+            var notB = GetNotB();
+            var intersection = new HashSet<int>(notB);
+            intersection.IntersectWith(_setC);
+            var sortedResult = intersection.ToList();
+            sortedResult.Sort();
+            return sortedResult;
+        }
+
+        private void _7_Click(object sender, RoutedEventArgs e)
+        {
+            var result = GetNotAIntersectBOrAIntersectNotC();
+            TextBox7.Text = $"¬A ∩ B ∪ A ∩ ¬C = {{{string.Join(", ", result)}}}";
+        }
+
+        private List<int> GetNotAIntersectBOrAIntersectNotC()
+        {
+            var part1 = GetNotAIntersectB();
+            var part2 = GetAIntersectNotC();
+            var unionResult = new HashSet<int>(part1);
+            unionResult.UnionWith(part2);
+            var sortedResult = unionResult.ToList();
+            sortedResult.Sort();
+            return sortedResult;
+        }
+
+        private void _8_Click(object sender, RoutedEventArgs e)
+        {
+            var result = GetFinalUnion();
+            TextBox8.Text = $"¬A ∩ B ∪ A ∩ ¬C ∪ ¬B ∩ C = {{{string.Join(", ", result)}}}";
+        }
+
+        private List<int> GetFinalUnion()
+        {
+            var part1 = GetNotAIntersectB();
+            var part2 = GetAIntersectNotC();
+            var part3 = GetNotBIntersectC();
+            var unionResult = new HashSet<int>(part1);
+            unionResult.UnionWith(part2);
+            unionResult.UnionWith(part3);
+            var sortedResult = unionResult.ToList();
+            sortedResult.Sort();
+            return sortedResult;
+        }
+
+        private void InitializeSets()
+        {
+            _setA = new HashSet<int> { 191, 194, 197, 200, 203 };
+            _setB = new HashSet<int> { 192, 195, 191, 201, 204 };
+            _setC = new HashSet<int> { 193, 196, 199, 203, 205 };
+
+            _universalSet = new HashSet<int>(_setA);
+            _universalSet.UnionWith(_setB);
+            _universalSet.UnionWith(_setC);
+        }
+
+        private void DisplaySets()
+        {
+            TextBoxSetA.Text = $"A = {{ {string.Join(", ", _setA)} }}";
+            TextBoxSetB.Text = $"B = {{ {string.Join(", ", _setB)} }}";
+            TextBoxSetC.Text = $"C = {{ {string.Join(", ", _setC)} }}";
+            TextBoxUniversal.Text = $"Universal = {{ {string.Join(", ", _universalSet)} }}";
+        }
+    }
+}
---
+++ Федурин/Множества/Algos2Lab/Algos2Lab/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Algos2Lab
+{
+    public partial class MainWindow : Window
+    {
+        private readonly List<int> _setA = new List<int> { 1, 2, 3, 4, 9 };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DisplayRelation();
+            DisplayProperties();
+        }
+
+        private void DisplayRelation()
+        {
+            var data = new ObservableCollection<RelationRow>();
+
+            foreach (var b in _setA)
+            {
+                var row = new RelationRow
+                {
+                    B = b,
+                    A1 = $"(1, {b})",
+                    A1Highlight = (1 == b * b),
+                    A2 = $"(2, {b})",
+                    A2Highlight = (2 == b * b),
+                    A3 = $"(3, {b})",
+                    A3Highlight = (3 == b * b),
+                    A4 = $"(4, {b})",
+                    A4Highlight = (4 == b * b),
+                    A9 = $"(9, {b})",
+                    A9Highlight = (9 == b * b)
+                };
+
+                data.Add(row);
+            }
+
+            RelationDataGrid.ItemsSource = data;
+        }
+
+        private void DisplayProperties()
+        {
+            var relation = new List<Tuple<int, int>>();
+            foreach (var a in _setA)
+            {
+                foreach (var b in _setA)
+                {
+                    if (a == b * b)
+                    {
+                        relation.Add(Tuple.Create(a, b));
+                    }
+                }
+            }
+
+            bool reflexive = _setA.All(a => relation.Contains(Tuple.Create(a, a)));
+
+            bool antiReflexive = _setA.All(a => !relation.Contains(Tuple.Create(a, a)));
+
+            bool symmetric = relation.All(pair => relation.Contains(Tuple.Create(pair.Item2, pair.Item1)));
+
+            bool antiSymmetric = true;
+            foreach (var pair in relation)
+            {
+                if (relation.Contains(Tuple.Create(pair.Item2, pair.Item1)) && pair.Item1 != pair.Item2)
+                {
+                    antiSymmetric = false;
+                    break;
+                }
+            }
+
+            bool transitive = true;
+            foreach (var pair1 in relation)
+            {
+                foreach (var pair2 in relation)
+                {
+                    if (pair1.Item2 == pair2.Item1 && !relation.Contains(Tuple.Create(pair1.Item1, pair2.Item2)))
+                    {
+                        transitive = false;
+                        goto Exit;
+                    }
+                }
+            }
+        Exit:
+            label1.Content = $"Рефлексивное: {reflexive}\n" +
+                             $"Антирефлексивное: {antiReflexive}\n" +
+                             $"Симметричное: {symmetric}\n" +
+                             $"Антисимметричное: {antiSymmetric}\n" +
+                             $"Транзитивное: {transitive}";
+        }
+
+        public class RelationRow
+        {
+            public int B { get; set; }
+            public string A1 { get; set; }
+            public bool A1Highlight { get; set; }
+            public string A2 { get; set; }
+            public bool A2Highlight { get; set; }
+            public string A3 { get; set; }
+            public bool A3Highlight { get; set; }
+            public string A4 { get; set; }
+            public bool A4Highlight { get; set; }
+            public string A9 { get; set; }
+            public bool A9Highlight { get; set; }
+        }
+    }
+    public class BackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isHighlighted)
+            {
+                return isHighlighted ? Brushes.LightGreen : Brushes.White;
+            }
+            return Brushes.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
